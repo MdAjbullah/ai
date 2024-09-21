424d801fa4f464374da6ad84547094b385e268b2
Mdajbullah <mdajbullahmansuri@gmail.com>
2024-09-22 03:02:03 +0530

    md

--- index.js
+++ index.js
@@ -59,9 +59,9 @@ btn.addEventListener('click', () => {
 });
 
 function takeCommand(message) {
+    message = message.toLowerCase();
     btn.style.display = 'flex';
     voice.style.display = 'none';
-    message = message.toLowerCase();
 
     // Basic responses
     if (message.includes('hello')) {
@@ -74,8 +74,35 @@ function takeCommand(message) {
         speak('I am Miki.');
     } else if (message.includes('what is the time')) {
         speak('The time is ' + new Date().getHours() + ":" + new Date().getMinutes());
+
+    // Open websites
+    } else if (message.includes('open google')) {
+        window.open('https://www.google.com');
+    } else if (message.includes('open youtube')) {
+        window.open('https://www.youtube.com');
+    } else if (message.includes('open facebook')) {
+        window.open('https://www.facebook.com');
     }
 
+    // JavaScript concepts
+    else if (message.includes('what is javascript')) {
+        speak('JavaScript is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is widely used for client-side and server-side web development.');
+    } else if (message.includes('what is a variable in javascript')) {
+        speak('A variable is a container for storing data values. You can think of variables as a box that holds data.');
+    } else if (message.includes('what are the different types of variables in javascript')) {
+        speak('There are three types of variables in JavaScript: var, let, and const.');
+    } else if (message.includes('what is a function in javascript')) {
+        speak('A function is a block of code that can be defined once and executed or invoked multiple times.');
+    } else if (message.includes('what is an array in javascript')) {
+        speak('An array is a special variable that can hold more than one value at a time.');
+    } else if (message.includes('what is an object in javascript')) {
+        speak('An object is a collection of key-value pairs, where each key is a string and each value is any data type.');
+    } else if (message.includes('what is the difference between null and undefined')) {
+        speak('Null is an assignment value that represents no value, while undefined means a variable has been declared but has not yet been assigned a value.');
+    } else if (message.includes('what is scope in javascript')) {
+        speak('Scope determines the accessibility of variables. JavaScript has global scope, function scope, and block scope.');
+  
+    }
     // Open websites
     else if (message.includes('open google')) {
         window.open('https://www.google.com');
